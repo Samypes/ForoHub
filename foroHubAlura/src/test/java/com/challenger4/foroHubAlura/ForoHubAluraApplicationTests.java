ce5ae76eb4d400cd1e6f3e45d2b76389c8de79aa
Samypes <labuzeria@gmail.com>
2024-07-16 08:59:51 -0500

    Terminando programa foro hub

---
+++ foroHubAlura/src/test/java/com/challenger4/foroHubAlura/ForoHubAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenger4.foroHubAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
